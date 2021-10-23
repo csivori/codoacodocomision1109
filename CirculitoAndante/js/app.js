0022707ca811c9537bc8a5f6ca7b8dfeed78678c
csivori <carlos.sivori@gmail.com>
2021-10-22 23:41:57 -0300

    Circulitos Andantes v4

--- CirculitoAndante/js/app.js
+++ CirculitoAndante/js/app.js
@@ -1,13 +1,4 @@
-const maxTiempo = 30;
-const radioElemento = 9;
-let zona = document.getElementById("zona");
-anchoZona = zona.offsetWidth;
-altoZona = zona.offsetHeight;
-anchoZonaLimite = anchoZona - (radioElemento * 2) - 6;
-altoZonaLimite = altoZona - (radioElemento * 2) - 6;
-console.log(`Zona [Ancho:${anchoZonaLimite} / Alto:${altoZonaLimite}]`);
-let elementos = [];
-const colores = ['red', 'green', 'blue', 'yellow', 'magenta', 'white']
+const maxTiempo = 100000;
 
 class Zona {
     constructor(id, posX, posY, ancho, alto, color, idPadre, elementos) {
@@ -59,21 +50,10 @@ class Elemento {
         if ((this.posX + this.incX + (this.radio*2) > maxX) || (this.posX + this.incX < minX)) { this.incX = -this.incX };
         this.posX += this.incX;
         if ((this.posY + this.incY + (this.radio*2) > maxY) || (this.posY + this.incY < minY)) { this.incY = -this.incY };
-        // if (this.id == 1) { console.log(`       this1Y:${this.posY} -> ${this.posY += this.incY}`) }
         this.posY += this.incY;
     }
 };
 
-let zona1 = new Zona(1, 50, 50, 200, 180, 'lightgray', "zona",
-    new Elementos([new Elemento(1, 8, 10, 0, 1, 1, 'red'),
-                   new Elemento(2, 8, 0, 0, 2, 1, 'green'),
-                   new Elemento(4, 12, 30, 10, 3, 3, 'magenta'),
-                   new Elemento(5, 8, 30, 10, 1, 0, 'white'),
-                   new Elemento(3, 16, 20, 30, 1, 2, 'blue')]));
-
-zona1.mostrar();
-var intervalId = setInterval(mover1T, 50);
-tiempo = 0;
 function mover1T() {
     if (tiempo > maxTiempo) { clearInterval(intervalId); }
     else {
@@ -82,3 +62,13 @@ function mover1T() {
         zona1.mostrar();
     };
 }
+
+let zona1 = new Zona(1, 50, 25, 250, 180, 'lightgray', "zona",
+    new Elementos([new Elemento(1, 8, 10, 0, 1, 1, 'red'),
+                   new Elemento(2, 8, 0, 0, 2, 1, 'green'),
+                   new Elemento(3, 16, 20, 30, 1, 2, 'blue'),
+                   new Elemento(4, 12, 30, 10, 3, 3, 'magenta'),
+                   new Elemento(5, 8, 30, 10, 1, 0, 'white')]));
+zona1.mostrar();
+var intervalId = setInterval(mover1T, 20);
+tiempo = 0;
